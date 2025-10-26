295dcd77a655ac43df15ebfbbe475bd48cf2d06b
gksals12 <yunhanmin@gmail.com>
2025-10-26 22:38:12 +0900

    Edit global.js ttf -> truetype

--- src/styles/global.js
+++ src/styles/global.js
@@ -18,37 +18,37 @@ const GlobalStyle = createGlobalStyle`
 
   @font-face {
     font-family: 'Daeojamjil';
-    src: url(${process.env.PUBLIC_URL}/assets/fonts/jamsil/TheJamsil1Thin.ttf) format('ttf');
+    src: url(${process.env.PUBLIC_URL}/assets/fonts/jamsil/TheJamsil1Thin.ttf) format('truetype');
     font-weight: 100;
     font-display: swap;
 }
     @font-face {
     font-family: 'Daeojamjil';
-    src: url(${process.env.PUBLIC_URL}/assets/fonts/jamsil/TheJamsil2Light.ttf) format('ttf');
+    src: url(${process.env.PUBLIC_URL}/assets/fonts/jamsil/TheJamsil2Light.ttf) format('truetype');
     font-weight: 300;
     font-display: swap;
 }
   @font-face {
     font-family: 'Daeojamjil';
-    src: url(${process.env.PUBLIC_URL}/assets/fonts/jamsil/TheJamsil3Regular.ttf) format('ttf');
+    src: url(${process.env.PUBLIC_URL}/assets/fonts/jamsil/TheJamsil3Regular.ttf) format('truetype');
     font-weight: 400;
     font-display: swap;
 }
   @font-face {
     font-family: 'Daeojamjil';
-    src: url(${process.env.PUBLIC_URL}/assets/fonts/jamsil/TheJamsil4Medium.ttf) format('ttf');
+    src: url(${process.env.PUBLIC_URL}/assets/fonts/jamsil/TheJamsil4Medium.ttf) format('truetype');
     font-weight: 500;
     font-display: swap;
 }
   @font-face {
     font-family: 'Daeojamjil';
-    src: url(${process.env.PUBLIC_URL}/assets/fonts/jamsil/TheJamsil5Bold.ttf) format('ttf');
+    src: url(${process.env.PUBLIC_URL}/assets/fonts/jamsil/TheJamsil5Bold.ttf) format('truetype');
     font-weight: 700;
     font-display: swap;
 }
   @font-face {
     font-family: 'Daeojamjil';
-    src: url(${process.env.PUBLIC_URL}/assets/fonts/jamsil/TheJamsil6ExtraBold.ttf) format('ttf');
+    src: url(${process.env.PUBLIC_URL}/assets/fonts/jamsil/TheJamsil6ExtraBold.ttf) format('truetype');
     font-weight: 800;
     font-display: swap;
 }
