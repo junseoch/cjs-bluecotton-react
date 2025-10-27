2a539c59a4f47faa707efb66ea77f98c671e9935
gksals12 <yunhanmin@gmail.com>
2025-10-28 08:51:01 +0900

    add map

--- src/pages/map/MapContainer.jsx
+++ src/pages/map/MapContainer.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { MapMarker } from 'react-kakao-maps-sdk';
+import { StyledMap } from './style';
 
 const MapContainer = () => {
+  const [latitude, setLatitude] = useState(37.44937029089704);
+  const [longitude, setLongitude] = useState(126.65436049042351);
+
   return (
     <div>
-      지도
+      <StyledMap center={{ lat: latitude, lng: longitude }} isPanto={true}>
+        <MapMarker position={{ lat: latitude, lng: longitude }} />
+      </StyledMap>
     </div>
   );
 };
